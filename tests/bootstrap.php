a299469b64505d6472c9a1aabd0e146d182ee102
Tomáš Vojík <vojik@wboy.cz>
2022-07-05 23:58:12 +0200

    :white_check_mark: Configure unit tests
    Signed-off-by: Tomáš Vojík <vojik@wboy.cz>

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ROOT', dirname(__DIR__).'/');
+const PRIVATE_DIR = ROOT.'tests/private/';
+const TMP_DIR = ROOT.'tests/tmp/';
+const LOG_DIR = ROOT.'tests/logs/';
+
+require_once ROOT.'vendor/autoload.php';
