c3dfc5ba06330fa857e8a4323bd938dd2673e97d
Tomáš Vojík <vojik@wboy.cz>
2024-01-09 13:52:06 +0100

    :zap: Cache parsed config
    Signed-off-by: Tomáš Vojík <vojik@wboy.cz>

--- src/Config.php
+++ src/Config.php
@@ -31,19 +31,22 @@ class Config
 		'ENV' => [],
 	];
 
-	public function __construct() {
+	private string $iniFile;
+	private string $neonFile;
+	private string $envFile;
+
+	public function __construct(
+		private readonly string $cacheDir = TMP_DIR
+	) {
 		$this->config['ENV'] = self::ENV_DEFAULTS;
 	}
 
 	/**
 	 * @return Config
 	 */
-	public static function getInstance() : Config {
+	public static function getInstance(string $cacheDir = TMP_DIR) : Config {
 		if (!isset(self::$instance)) {
-			self::$instance = new self();
-		}
-		if (!self::$instance->isInitialized()) {
-			self::$instance->init();
+			self::$instance = new self($cacheDir);
 		}
 		return self::$instance;
 	}
@@ -58,22 +61,27 @@ class Config
 	/**
 	 * Initializes APP configuration
 	 *
+	 * Checks config cache first. Should be only called once.
+	 *
+	 * @post Initialized flag is set
+	 * @post Config is cached into a cache file
+	 *
 	 * @return void
 	 */
 	public function init() : void {
-		if ($this->initialized) {
+		if ($this->initialized || $this->checkCache()) {
 			return;
 		}
 		$ini = $neon = [];
-		if (file_exists(PRIVATE_DIR.'config.ini')) {
-			$ini = parse_ini_file(PRIVATE_DIR.'config.ini', true);
+		if (($iniFile = $this->getIniFile()) !== '') {
+			$ini = parse_ini_file($iniFile, true);
 			if ($ini === false) {
 				$ini = [];
 			}
 		}
-		if (file_exists(PRIVATE_DIR.'config.neon')) {
+		if (($neonFile = $this->getNeonFile()) !== '') {
 			try {
-				$neon = Neon::decodeFile(PRIVATE_DIR.'config.neon');
+				$neon = Neon::decodeFile($neonFile);
 			} catch (Exception) {
 			}
 		}
@@ -89,6 +97,74 @@ class Config
 		$this->config['ENV'] = array_merge($this->config['ENV'], $_ENV, $env);
 
 		$this->initialized = true;
+		$this->saveCache();
+	}
+
+	/**
+	 * Check config cache file and if it exists and is valid, load the cache file into config
+	 *
+	 * @post If cache file is valid, it is loaded into config.
+	 *
+	 * @return bool If the cache file is valid and loaded
+	 */
+	public function checkCache() : bool {
+		$cacheFile = $this->cacheDir.'config.cache';
+		if (!file_exists($cacheFile)) {
+			return false;
+		}
+
+		// Check file updates
+		$modTime = filemtime($cacheFile);
+		$ini = $this->getIniFile();
+		if ($ini !== '' && filemtime($ini) > $modTime) {
+			return false;
+		}
+		$neon = $this->getNeonFile();
+		if ($neon !== '' && filemtime($neon) > $modTime) {
+			return false;
+		}
+		$env = $this->getEnvFile();
+		if ($env !== '' && filemtime($env) > $modTime) {
+			return false;
+		}
+
+		// Load cache
+		$content = file_get_contents($cacheFile);
+		if (!$content) {
+			return false;
+		}
+		/** @var false|array<string,array<string,string|numeric>|string|numeric> $config */
+		$config = unserialize($content, ['allowed_classes' => false]);
+		if ($config === false) {
+			return false;
+		}
+		$this->config = $config;
+
+		return true;
+	}
+
+	/**
+	 * @return string Absolute file path or empty string if the file does not exist
+	 */
+	public function getIniFile() : string {
+		$this->iniFile ??= file_exists(PRIVATE_DIR.'config.ini') ? PRIVATE_DIR.'config.ini' : '';
+		return $this->iniFile;
+	}
+
+	/**
+	 * @return string Absolute file path or empty string if the file does not exist
+	 */
+	public function getNeonFile() : string {
+		$this->neonFile ??= file_exists(PRIVATE_DIR.'config.neon') ? PRIVATE_DIR.'config.neon' : '';
+		return $this->neonFile;
+	}
+
+	/**
+	 * @return string Absolute file path or empty string if the file does not exist
+	 */
+	public function getEnvFile() : string {
+		$this->envFile ??= file_exists(ROOT.'.env') ? ROOT.'.env' : '';
+		return $this->envFile;
 	}
 
 	/**
@@ -103,7 +179,8 @@ class Config
 	/**
 	 * @param string|null $category
 	 *
-	 * @return ($category is null ? array<string,array<string,string|numeric>|numeric|string> : array<string,string|numeric>)
+	 * @return ($category is null ? array<string,array<string,string|numeric>|numeric|string> :
+	 *                    array<string,string|numeric>)
 	 */
 	public function getConfig(?string $category = null) : array {
 		if (!$this->initialized) {
@@ -118,6 +195,16 @@ class Config
 			return $return;
 		}
 		return $this->config;
+	}
+
+	/**
+	 * Cache loaded config into a file
+	 *
+	 * @return bool If save was successful
+	 */
+	public function saveCache() : bool {
+		$success = file_put_contents($this->cacheDir.'config.cache', serialize($this->config));
+		return $success !== false && $success > 0;
 	}
 
 }
